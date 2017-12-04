80398e6051638e01988a59198ad400fd0207c899
NgaPhan <ngabmgmail.com>
2017-12-04 23:20:37 +0700

    commit to repository

---
+++ frmDSSinhVien.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLDSV
+{
+    public partial class frmDSSinhVien : Form
+    {
+       
+        public frmDSSinhVien()
+        {
+            InitializeComponent();
+        }
+
+        private void lOPBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.lOPBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.dS);
+
+        }
+
+        private void frmSinhVien_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dS.LOP' table. You can move, or remove it, as needed.
+            this.LOPTableAdapter.Connection.ConnectionString = Program.connstr;
+            this.LOPTableAdapter.Fill(this.dS.LOP);
+            cmbMaLop.Enabled = false;
+            cmbTenLop.SelectedIndex = 0;
+            cmbKhoa.DataSource = Program.bds_dspm;  // sao chép bds_dspm đã load ở form đăng nhập  qua
+            cmbKhoa.DisplayMember = "TENCN";
+            cmbKhoa.ValueMember = "TENSERVER";
+            cmbKhoa.SelectedIndex = Program.mChinhanh;
+
+            if (Program.mGroup == "PGV")
+                cmbTenLop.Enabled = true;
+            else
+                cmbTenLop.Enabled = false;
+        }
+
+        private void cmbKhoa_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            if (cmbKhoa.SelectedValue.ToString() == "System.Data.DataRowView") return;
+            Program.servername = cmbKhoa.SelectedValue.ToString();
+
+            if (cmbKhoa.SelectedIndex != Program.mChinhanh)
+            {
+                Program.mlogin = Program.remotelogin;
+                Program.password = Program.remotepassword;
+            }
+            else
+            {
+                Program.mlogin = Program.mloginDN;
+                Program.password = Program.passwordDN;
+            }
+            if (Program.KetNoi() == 0)
+                MessageBox.Show("Lỗi kết nối về chi nhánh mới", "", MessageBoxButtons.OK);
+            else
+            {
+                this.LOPTableAdapter.Connection.ConnectionString = Program.connstr;
+                this.LOPTableAdapter.Fill(this.dS.LOP);
+
+            }
+        }
+
+        private void btnPreview_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            String strLenh = "EXEC SP_InDanhSachSinhVien N'" + cmbMaLop.Text.Trim() + "'";
+            //MessageBox.Show(strLenh);
+            dt = Program.ExecSqlDataTable(strLenh);
+            rptDSSV rp = new rptDSSV();
+
+            rp.SetDataSource(dt);
+            rp.SetParameterValue("tenLop",cmbTenLop.Text);
+            crystalReportViewer1.ReportSource = rp;
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
